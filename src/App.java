a4fb9d85664baba829fa0ae263397a107e7377be
Farbkecks <fabian.alich@gmx.de>
2022-06-14 11:41:56 +0200

    add print_board

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
+    static void print_board(String[] b){
+        for (int i=0; i<7; i+=3){
+            for (int y=0; y<3; y++){
+                System.out.print(b[i+y]);
+                if (y == 0 || y == 1){
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+            if (i==0 || i== 3){
+            System.out.println("-----");
+            }
+        }
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String[] board = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        print_board(board);
     }
 }
